a7d92e4cd73972601a50b98187582759ca689727
Felipe Hoyos <felipe02-98@hotmail.com>
2023-04-02 15:42:08 -0500

    first commit

---
+++ src/navigation/screens/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import {useNavigation} from "@react-navigation/core"
+import React from 'react';
+import {Button, Text, View} from 'react-native';
+
+export const HomeScreen = () => {
+
+    const navigation = useNavigation()
+    return (
+        <View>
+            <Text>HomeScreen</Text>
+        </View>
+    );
+}
