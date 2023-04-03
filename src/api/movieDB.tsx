6b008dd42bce6ff6919e2574d87b68dbc73bb302
Felipe Hoyos <felipe02-98@hotmail.com>
2023-04-02 19:19:57 -0500

    151. Obtener películas en cartelera

---
+++ src/api/movieDB.tsx
@@ -0,0 +1,12 @@
+//Aca realizare las peticiones a la API
+import axios from "axios";
+
+const movieDB = axios.create({
+    baseURL:"https://api.themoviedb.org/3/movie",
+    params:{
+       api_key:"9154a1cb8093afddf581316bd3d4f1aa",
+       language: "es-ES"
+    }
+})
+
+export default movieDB
--- src/navigation/screens/HomeScreen.tsx
+++ src/navigation/screens/HomeScreen.tsx
@@ -1,10 +1,19 @@
-import {useNavigation} from "@react-navigation/core"
-import React from 'react';
-import {Button, Text, View} from 'react-native';
+import React, { useEffect } from 'react';
+import {Text, View} from 'react-native';
+import movieDB from '../../api/movieDB';
 
 export const HomeScreen = () => {
 
-    const navigation = useNavigation()
+    useEffect(() => {
+      
+        movieDB.get("/now_playing")
+        .then(resp=>{
+            console.log(resp.data)
+        })
+     
+    }, [])
+    
+
     return (
         <View>
             <Text>HomeScreen</Text>
