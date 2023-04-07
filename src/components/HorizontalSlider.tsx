ce9eb1e1314822a1f058c3f702d3f0a1599ecccb
Felipe Hoyos <felipe02-98@hotmail.com>
2023-04-07 13:55:08 -0500

    157. Componente HorizontalSlider

---
+++ src/components/HorizontalSlider.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Movie } from '../interfaces/movieInterface';
+import { FlatList } from 'react-native-gesture-handler';
+import { MoviePoster } from './MoviePoster';
+
+interface Props {
+    title?: string;
+    movies: Movie[]
+}
+
+export const HorizontalSlider = ({ title, movies }: Props) => {
+    return (
+        <View style={{  
+        height: (title)? 260:220 ,
+        padding: (title)?0:10
+        }}>
+            {
+                title && <Text style={{ fontSize: 30, fontWeight: "bold", color: "black" }}>{title}</Text>
+            }
+            <FlatList
+                data={movies}
+                renderItem={({ item }: any) => (
+                    <MoviePoster movie={item} width={140} height={200} />
+                )}
+                keyExtractor={(item) => item.id.toString()}
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+            />
+        </View>
+
+    );
+}
--- src/navigation/screens/HomeScreen.tsx
+++ src/navigation/screens/HomeScreen.tsx
@@ -1,54 +1,46 @@
 import React from 'react';
-import {ActivityIndicator, Dimensions, Text, View} from 'react-native';
+import { ActivityIndicator, Dimensions, Text, View } from 'react-native';
 import { useMovies } from '../../hooks/useMovies';
 import { MoviePoster } from '../../components/MoviePoster';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import Carousel from 'react-native-snap-carousel';
 import { FlatList, ScrollView } from 'react-native-gesture-handler';
+import { HorizontalSlider } from '../../components/HorizontalSlider';
 
-const {width: windowWidht} = Dimensions.get("window")
+const { width: windowWidht } = Dimensions.get("window")
 
 export const HomeScreen = () => {
 
-    const {peliculasEnCine, isLoading} =useMovies()
-    const {top} =useSafeAreaInsets()
-    
-    if(isLoading){
-        return(
-            <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
-                <ActivityIndicator color="red" size={100}/>
+    const { peliculasEnCine, isLoading } = useMovies()
+    const { top } = useSafeAreaInsets()
+
+    if (isLoading) {
+        return (
+            <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+                <ActivityIndicator color="red" size={100} />
             </View>
         )
     }
     return (
         <ScrollView>
-             <View style={{marginTop: top+20}}>
+            <View style={{ marginTop: top + 20 }}>
 
-{/* Carosel Principal */}
-<View style={{height: 440}}>
-<Carousel 
-data={peliculasEnCine}
-renderItem={({item}:any)=> <MoviePoster movie={item}/>}
-sliderWidth={windowWidht}
-itemWidth={300}/>
-</View>
+                {/* Carosel Principal */}
+                <View style={{ height: 440 }}>
+                    <Carousel
+                        data={peliculasEnCine}
+                        renderItem={({ item }: any) => <MoviePoster movie={item} />}
+                        sliderWidth={windowWidht}
+                        itemWidth={300} 
+                        inactiveSlideOpacity={0.9}/>
+                </View>
 
-{/* Peliculas populares */}
+                {/* Peliculas populares */}
 
-<View style={{backgroundColor:"red", height: 260}}>
-    <Text style={{fontSize:30, fontWeight:"bold", color: "black"}}>En cine</Text>
-    <FlatList 
-        data={peliculasEnCine}
-        renderItem={({item}:any)=> (
-            <MoviePoster movie={item} width={140} height={200}/>
-            )}
-        keyExtractor={(item)=>item.id.toString()}
-        horizontal={true}
-        showsHorizontalScrollIndicator={false}
-    />
-</View>
-</View>
+                    <HorizontalSlider movies={peliculasEnCine} title='En cine' />
+
+            </View>
         </ScrollView>
-       
+
     );
 }
